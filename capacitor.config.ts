1d34ce625650b9da8384283fe88083cbc310159a
CHRISTOPHER BIJU <149104247+christopher-biju@users.noreply.github.com>
2026-06-09 09:58:13 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.b88f64bf89f84656938b98affac69f0e',
+  appName: 'attendace-watch',
+  webDir: 'dist',
+};
+
+export default config;
